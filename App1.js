893be5f2e1a2931c6f87e5495e42f727d6bb8120
shubhgithubrit <sg4030383@gmail.com>
2023-03-28 10:02:15 +0530

    first commit

---
+++ App1.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App1 = () => {
+  return (
+    <View>
+      <Text>App1</Text>
+    </View>
+  )
+}
+
+export default App1
+
+const styles = StyleSheet.create({})
